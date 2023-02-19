3e4cc286264ef74fce18099511d7a8966634f06c
Johnny <johnny_s.o@hotmail.com>
2023-02-19 13:12:06 -0300

    Adicionando advinhação do número secreto

--- maior_menor.rb
+++ maior_menor.rb
@@ -5,3 +5,10 @@ puts puts;
 puts "Vamos começar o jogo, #{nome} ";
 puts "Escolhendo número secreto entre 0 e 200..."; 
 numero_secreto = 16;
+puts "Número escolhido! Vamos advinhar qual é?"
+puts puts
+puts "Tentativa 1"
+puts "Qual é o meu  número secreto?"
+chute = gets.chomp
+puts "Você chutou o número #{chute}... Será que você acertou?"
+puts chute.to_i == numero_secreto;
