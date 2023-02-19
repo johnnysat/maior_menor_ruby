d080d76853e5c5f885969b93a49b4b3b89fdde28
Johnny <johnny_s.o@hotmail.com>
2023-02-19 12:55:49 -0300

    Adicionando número secreto

--- maior_menor.rb
+++ maior_menor.rb
@@ -1,5 +1,7 @@
-puts "Bem vindo ao jogo da advinhação"
-puts "Qual é seu nome?"
+puts "Bem vindo ao jogo da advinhação";
+puts "Qual é seu nome?";
 nome = gets.chomp;
-puts puts
-puts "Vamos começar o jogo, #{nome} "
+puts puts;
+puts "Vamos começar o jogo, #{nome} ";
+puts "Escolhendo número secreto entre 0 e 200..."; 
+numero_secreto = 16;
