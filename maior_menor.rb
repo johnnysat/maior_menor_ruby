8d073bb6bb62deff8222ad9c0294265c1c92203a
Johnny <johnny_s.o@hotmail.com>
2023-02-20 09:48:51 -0300

    Adicionado um for como laço de repetição para varias tentativas

--- maior_menor.rb
+++ maior_menor.rb
@@ -7,18 +7,21 @@ puts "Escolhendo número secreto entre 0 e 200...";
 numero_secreto = 16;
 puts "Número escolhido! Vamos advinhar qual é?"
 puts "\n"
-puts "Tentativa 1"
-puts "Qual é o meu  número secreto?"
-chute = gets.chomp.to_i
-puts "Você chutou o número #{chute}... Será que você acertou?"
-acertou = numero_secreto == chute;
 
-if acertou
-  puts "Acertou!"
-else
-    if chute > numero_secreto
-      puts "você chutou alto, tente mais baixo!"
-    else chute < numero_secreto
-      puts "Você chutou baixo, tente mais alto!"
-    end
+total_tentativas = 5
+for tentativa in 1..total_tentativas
+  puts "Tentativa #{tentativa.to_s} de #{total_tentativas.to_s}"
+  puts "Qual é o meu número secreto?"
+  chute = gets.chomp.to_i
+  puts "Você chutou o número #{chute}... Será que você acertou?"
+  acertou = numero_secreto == chute
+
+  if acertou
+    puts "Acertou!"
+  elsif chute > numero_secreto
+    puts "Você chutou alto, tente mais baixo!"
+  elsif chute < numero_secreto
+    puts "Você chutou baixo, tente mais alto!"
+  end
 end
+
