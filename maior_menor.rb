45c2309b524695a8c3690b01119047c739f53756
Johnny <johnny_s.o@hotmail.com>
2023-02-20 07:38:21 -0300

    Adicionando condicionais para respostas

--- maior_menor.rb
+++ maior_menor.rb
@@ -1,14 +1,19 @@
 puts "Bem vindo ao jogo da advinhação";
 puts "Qual é seu nome?";
 nome = gets.chomp;
-puts puts;
+puts "\n"
 puts "Vamos começar o jogo, #{nome} ";
 puts "Escolhendo número secreto entre 0 e 200..."; 
 numero_secreto = 16;
 puts "Número escolhido! Vamos advinhar qual é?"
-puts puts
+puts "\n"
 puts "Tentativa 1"
 puts "Qual é o meu  número secreto?"
-chute = gets.chomp
+chute = gets.chomp.to_i
 puts "Você chutou o número #{chute}... Será que você acertou?"
-puts chute.to_i == numero_secreto;
+
+if numero_secreto == chute
+  puts "Acertou!"
+  else
+    puts "Errou"
+end
