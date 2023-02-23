f4373defe563f14de76ef34ca85c35435755431c
Johnny <johnny_s.o@hotmail.com>
2023-02-23 18:53:53 -0300

    Corrigindo erro de escopo e tirando o for de dentro de um método

--- maior_menor.rb
+++ maior_menor.rb
@@ -8,34 +8,28 @@ end
 
 def sorteia_numero_secreto
   puts "Escolhendo número secreto entre 0 e 10..."; 
-  numero_secreto = rand(11);
+  numero_sorteado = rand(11);
 end
 
 boas_vindas;
 numero_secreto = sorteia_numero_secreto;
-nome = boas_vindas;
 
+puts "Número escolhido! Vamos advinhar qual é?"
+puts "\n"
+total_tentativas = 5
+for tentativa in 1..total_tentativas
+  puts "Tentativa #{tentativa.to_s} de #{total_tentativas.to_s}"
+  puts "Qual é o meu número secreto?"
+  chute = gets.chomp.to_i
+  puts "Você chutou o número #{chute}... Será que você acertou?"
+  acertou = numero_secreto == chute
 
-def jogador_tentativas
-  puts "Número escolhido! Vamos advinhar qual é?"
-  puts "\n"
-  total_tentativas = 5
-  for tentativa in 1..total_tentativas
-    puts "Tentativa #{tentativa.to_s} de #{total_tentativas.to_s}"
-    puts "Qual é o meu número secreto?"
-    chute = gets.chomp.to_i
-    puts "Você chutou o número #{chute}... Será que você acertou?"
-    acertou = numero_secreto == chute
-
-    if acertou
-      puts "Acertou! Você venceu o jogo, #{nome}!"
-      break
-      elsif chute > numero_secreto
-      puts "Você chutou alto, tente mais baixo!"
-     elsif chute < numero_secreto
-      puts "Você chutou baixo, tente mais alto!"
-    end
+  if acertou
+    puts "Acertou! Você venceu o jogo, #{nome}!"
+    break
+    elsif chute > numero_secreto
+    puts "Você chutou alto, tente mais baixo!"
+    elsif chute < numero_secreto
+    puts "Você chutou baixo, tente mais alto!"
   end
 end
-
-jogador_tentativas
