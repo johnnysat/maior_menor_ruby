cba5adbe2c243ec76a9cf4275f7cbf58908e40e6
Johnny <johnny_s.o@hotmail.com>
2023-02-23 19:10:56 -0300

    Criando função para pedir o número

--- maior_menor.rb
+++ maior_menor.rb
@@ -4,11 +4,20 @@ def boas_vindas
   nome = gets.chomp;
   puts "\n"
   puts "Vamos começar o jogo, #{nome} ";
+  nome
 end
 
 def sorteia_numero_secreto
   puts "Escolhendo número secreto entre 0 e 10..."; 
   numero_sorteado = rand(11);
+end
+
+def pede_numero(tentativa, total_tentativas)
+  puts "Tentativa #{tentativa.to_s} de #{total_tentativas.to_s}"
+  puts "Qual é o meu número secreto?"
+  chute = gets.chomp.to_i
+  puts "Você chutou o número #{chute}... Será que você acertou?"
+  chute
 end
 
 nome = boas_vindas
@@ -19,18 +28,15 @@ puts "\n"
 total_tentativas = 5
 
 for tentativa in 1..total_tentativas
-  puts "Tentativa #{tentativa.to_s} de #{total_tentativas.to_s}"
-  puts "Qual é o meu número secreto?"
-  chute = gets.chomp.to_i
-  puts "Você chutou o número #{chute}... Será que você acertou?"
+  chute = pede_numero(tentativa, total_tentativas)
   acertou = numero_secreto == chute
 
   if acertou
     puts "Acertou! Você venceu o jogo!"
     break
-    elsif chute > numero_secreto
+  elsif chute > numero_secreto
     puts "Você chutou alto, tente mais baixo!"
-    elsif chute < numero_secreto
+  elsif chute < numero_secreto
     puts "Você chutou baixo, tente mais alto!"
   end
 end
